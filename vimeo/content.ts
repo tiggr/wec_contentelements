ef597f7bbc1fcf94ce3afc051ca650d7f8601892
Daniel Schöne <daniel@schoene.it>
2017-05-11 15:13:04 +0200

    Use https for vimeo video player

--- vimeo/content.ts
+++ vimeo/content.ts
@@ -7,8 +7,8 @@ tt_content.vimeo {
 		<object width="{t3datastructure : pi_flexform->width}" height="{t3datastructure : pi_flexform->height}">
 			<param name="allowfullscreen" value="true" />
 			<param name="allowscriptaccess" value="always" />
-			<param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id={t3datastructure : pi_flexform->vimeoID}&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" />
-			<embed src="http://vimeo.com/moogaloop.swf?clip_id={t3datastructure : pi_flexform->vimeoID}&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="{t3datastructure : pi_flexform->width}" height="{t3datastructure : pi_flexform->height}"></embed>
+			<param name="movie" value="https://vimeo.com/moogaloop.swf?clip_id={t3datastructure : pi_flexform->vimeoID}&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" />
+			<embed src="https://vimeo.com/moogaloop.swf?clip_id={t3datastructure : pi_flexform->vimeoID}&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="{t3datastructure : pi_flexform->width}" height="{t3datastructure : pi_flexform->height}"></embed>
 		</object>
 	)
 	20.insertData = 1
